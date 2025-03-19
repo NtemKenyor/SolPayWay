700ab3a3a0ec5720ae1d88b9dcf86288fad1ae1f
NtemKenyor <nkenyor@gmail.com>
2025-03-19 08:08:53 +0100

    testing and payment stuffs

--- script/sdk.js
+++ script/sdk.js
@@ -45,7 +45,7 @@ class SolPayWay {
         // this.network = this.networkRpcMap[network];
         this.network_name = network;
         this.server = (server === 'local') ? 'http://localhost:3000/SolPayWay/backend' : 'https://roynek.com//SolPayWay/backend';
-        this.verification_link = server + '/verify-payment';
+        this.verification_link = this.server + '/verify-payment';
         this.custom = (custom) ? custom : {
             title: 'My Website Payment',
             description: 'Payment for services',
